d27a10960a7fa7b5a14863d7e65b5bd563cf788e
khaoulafs <khaoulafsihi@gmail.com>
2024-05-30 13:06:46 -0700

    Tasks

---
+++ src/MarkdownEditor.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Editor from "./MarkdownEditor/Editor";
+
+function MarkdownEditor() {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+}
+
+export default MarkdownEditor;
+
